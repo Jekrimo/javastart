38fc516ed8567252506bb0e177ac620508358f6d
jekrimo <jekrimo@gmail.com>
2016-09-30 14:46:23 -0600

    first Java Tests

---
+++ src/AnewTest.java
@@ -0,0 +1,76 @@
+/**
+ * Created by JeKriMo on 9/28/2016.
+ */
+import java.util.Scanner;
+public class AnewTest {
+    public static void main(String[] args){
+//        System.out.println("Tell Me THings!");
+////        int something = 4;
+////        int gear = 13;
+////        System.out.println(something * gear);
+////        int anew = gear + something;
+////        System.out.println(anew);
+////        int taken = anew - 3;
+////        System.out.println(taken);
+////        System.out.println("more words" + " " + anew);
+//
+//        Scanner jeffFart = new Scanner(System.in);
+//        double fnum, snum, answer;
+//        System.out.print("enter num: ");
+//        fnum = jeffFart.nextDouble();
+//        System.out.print("enter num: ");
+//        snum = jeffFart.nextDouble();
+//        Scanner inputmonth = new Scanner(System.in);
+//        System.out.print("Enter Month Number:");
+//        int month = inputmonth.nextInt();
+//        String monthString;
+//        switch (month) {
+//            case 1:  monthString = "January";
+//                break;
+//            case 2:  monthString = "February";
+//                break;
+//            case 3:  monthString = "March";
+//                break;
+//            case 4:  monthString = "April";
+//                break;
+//            case 5:  monthString = "May";
+//                break;
+//            case 6:  monthString = "June";
+//                break;
+//            case 7:  monthString = "July";
+//                break;
+//            case 8:  monthString = "August";
+//                break;
+//            case 9:  monthString = "September";
+//                break;
+//            case 10: monthString = "October";
+//                break;
+//            case 11: monthString = "November";
+//                break;
+//            case 12: monthString = "December";
+//                break;
+//            default: monthString = "Invalid month";
+//                break;
+//        }
+//        System.out.println(monthString);
+//        answer = fnum * snum;
+//        System.out.println(answer);
+////        if(answer % 4 == 0){
+////            System.out.println("It worked!!!!!!!!!!!!!!!");
+////        }
+////        else{
+////            System.out.println("It didn't work yo!!");
+////        }
+//
+//        int i;
+//        int a;
+//        int temp;
+//        for(i = 0; i <= "six"; i++){
+//            for(a = 1; a < "four"; a++) {
+//                System.out.println("this one is " + (i * a));
+//            }
+//        }
+        
+    }
+}
+
